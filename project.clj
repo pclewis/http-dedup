edc08df77da87a91b5767bf8b782ed303e764885
Philip Lewis <pcl@pclewis.com>
2014-03-16 18:22:57 -0400

    fix license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject http-dedup "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Proprietary"
+            :url "http://dctf.ytmnd.com"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [com.taoensso/timbre "3.1.6"]]
