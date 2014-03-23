bbbad5bf12047ca6b6053bca51b45e5388a4aefc
Philip Lewis <pcl@pclewis.com>
2014-03-23 16:26:07 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject http-dedup "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Proprietary"
-            :url "http://dctf.ytmnd.com"}
+  :description "Transparent reverse proxy for deduplicating HTTP requests."
+  :url "https://github.com/pclewis/http-dedup"
+  :license {:name "CC0"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [org.clojure/tools.cli "0.3.1"]
